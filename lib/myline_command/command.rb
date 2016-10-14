3b49f1530f268ed688779566e63bae6046294123
tomcha <tomcha@tomcha.net>
2016-10-14 10:17:28 +0900

    傘予報機能のコマンドラインアプリ部分実装

--- lib/myline_command/command.rb
+++ lib/myline_command/command.rb
@@ -1,10 +1,29 @@
-module MylilneCommand
+require 'json'
+require 'yaml'
+require 'line/bot'
+
+module MylineCommand
   class Command
-    def self.start_unbrella_advice
-      ua = MylineCommand::UnbrellaAdviser.new
-      ua.get_umbrella_data
+
+    def initialize
+      config_txt = YAML.load_file("config.yml")
+      ENV["LINE_CHANNEL_SECRET"] = config_txt["line"]["line_channel_secret"]
+      ENV["LINE_CHANNEL_TOKEN"] = config_txt["line"]["line_channel_token"]
+      ENV["LINE_USERID"] = config_txt["line"]["line_uid"]
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
     end
-    def self.start_dust_callender
+    
+    def start_unbrella_advice
+      ua = MylineCommand::UnbrellaAdviser.new
+      message = ua.get_umbrella_data
+      @client.push_message(ENV["LINE_USERID"], message)
+      "OK"
+    end
+
+    def start_dust_callender
     end
   end
 end

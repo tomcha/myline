f8bc34a93d5128c211dc0f277f491af4d5011df6
tomcha <tomcha@tomcha.net>
2016-12-28 20:41:56 +0900

    dbのフィールド追加修正

--- db/schema.sqlite3.sql
+++ db/schema.sqlite3.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS tasks (
   id INTEGER PRIMARY KEY NOT NULL,
   userid text NOT NULL,
+  task_text text NOT NULL,
   remind_datetime text NOT NULL,
   deleted_flag INTEGER NOT NULL,
   created_at text NOT NULL

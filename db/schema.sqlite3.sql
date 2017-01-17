706edc3b3f72f08bed078012a0cc6ea2ee0ee1e6
tomcha <tomcha@tomcha.net>
2017-01-17 12:39:58 +0900

    タスクdbスキーマ変更

--- db/schema.sqlite3.sql
+++ db/schema.sqlite3.sql
@@ -1,8 +1,10 @@
+DROP TABLE tasks;
 CREATE TABLE IF NOT EXISTS tasks (
   id INTEGER PRIMARY KEY NOT NULL,
   userid text NOT NULL,
   task_text text NOT NULL,
   remind_datetime text NOT NULL,
+  task_num INTEGER ,
   deleted_flag INTEGER NOT NULL,
   created_at text NOT NULL
 );

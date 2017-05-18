a0decdd04fae61edfcf97a2ba77c65571076acdc
tomcha <tomcha@tomcha.net>
2017-05-18 12:56:10 +0900

    傘指数スクレイピング正規表現を改修

--- lib/myline_command/unbrella_adviser.rb
+++ lib/myline_command/unbrella_adviser.rb
@@ -12,22 +12,25 @@ module MylineCommand
       uri = URI.parse(@targeturl)
       res = Net::HTTP.get(uri)
       res.force_encoding("utf-8")
-      res =~ /<dl id="exponentLargeLeft">(.+?)<\/dl>/m
+      res =~ /<section class="today-weather"><!-- 今日の天気 -->(.+?)<\/section>/m
       today_html = $1
-      today_html =~ /alt="指数:(.+?):(.+?)"/
+      today_html =~ />指数:(.+?)</
       @today_rainypercent = $1
-      @today_umbrella_advice = $2
+      today_html =~ /indexes-telop-1">(.+?)<\/p>/
+      @today_umbrella_advice = $1
 
-      res =~ /<dl id="exponentLargeRight">(.+?)<\/dl>/m
+      res =~ /<section class="today-weather"><!-- 今日の天気 -->(.+?)<\/section>/m
       tomorrow_html = $1
-      tomorrow_html =~ /alt="指数:(.+?):(.+?)"/
+      tomorrow_html =~ />指数:(.+?)</
       @tomorrow_rainypercent = $1
-      @tomorrow_umbrella_advice = $2
+      tomorrow_html =~ /indexes-telop-1">(.+?)<\/p>/
+      @tomorrow_umbrella_advice = $1
 
       umbrella_data = {today_rainypercent: @today_rainypercent,
                        today_umbrella_advice: @today_umbrella_advice,
                        tomorrow_rainypercent: @tomorrow_rainypercent,
                        tomorrow_umbrella_advice: @tomorrow_umbrella_advice}
+      p umbrella_data
       return umbrella_data
     end
   end

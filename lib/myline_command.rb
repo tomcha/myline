96c2a57c6e80093b8489b7f777b6547e8bf9c653
tomcha <tomcha@tomcha.net>
2020-02-29 16:01:18 +0900

    ヒブワクチンサーチのモジュール読み込み設定

--- lib/myline_command.rb
+++ lib/myline_command.rb
@@ -2,6 +2,7 @@ require 'myline'
 require 'myline_command/command'
 require 'myline_command/unbrella_adviser'
 require 'myline_command/dust_calender'
+require 'myline_command/hib_search'
 
 module MylineCommand
 end

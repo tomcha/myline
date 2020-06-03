fc323989561df64aa3316b2877136c3e74ac14fc
tomcha <tomcha@tomcha.net>
2020-06-03 13:48:42 +0900

    幼稚園URLから告知部分のソース取得メソッドを追加

--- lib/myline_command/assumption_notice.rb
+++ lib/myline_command/assumption_notice.rb
@@ -2,4 +2,15 @@ require 'net/http'
 require 'myline/secret'
 
 module MylineCommand
+  def initialize()
+    @targeturl = Myline::ASSUMPTION_URL
+  end
+
+  def get_html_source()
+    uri = URI.Parse(@targeturl)
+    response = Net::HTTP.get(uri)
+    response.force_encoding("utf-8")
+    notice = response =~ /<div id="news-wrap">.+?<\/div>/
+    @array = notice.scan(/<dt class="news">.+?<\/dd>)
+  end
 end
